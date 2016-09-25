d505288efd6ddb2591bf38724d2265d068d46dfd
ruanhaojian <610708075@qq.com>
2016-09-25 21:07:01 +0800

    nothing

--- src/third-party/bootstrap.js
+++ src/third-party/bootstrap.js
@@ -1,6 +1,4 @@
-/**
- * Created by dinhquangtrung on 11/10/15.
- */
+
 
 // import 'script!../../bower_components/jquery/dist/jquery.js';
 // import 'script!../../bower_components/bootstrap/dist/js/bootstrap.js';
